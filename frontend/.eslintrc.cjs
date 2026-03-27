6d896689d6e8a2e15785dbe528545992d883ece8
casudo <hello@kida.one>
2026-03-27 22:09:45 +0100

    Added ESLint config and TypeScript type definitions

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+}
